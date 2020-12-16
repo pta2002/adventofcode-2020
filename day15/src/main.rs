5457528c4bf09a2b2262f9fb3f7c8f21ae2fc2a3
Pedro Alves <pta2002@pta2002.com>
2020-12-16 23:37:13 +0000

    Day 15

--- day15/src/main.rs
+++ day15/src/main.rs
@@ -18,49 +18,42 @@ impl State {
 
     fn step(&mut self) -> usize {
         if self.iteration < self.numbers.len() {
-            self.last_spoken.insert(self.numbers[self.iteration], self.iteration);
+            if let Some(i) = self.prev {
+                self.last_spoken.insert(i, self.iteration);
+            }
+
             self.prev = Some(self.numbers[self.iteration]);
             self.iteration += 1;
-            self.numbers[self.iteration - 1]
+            return self.numbers[self.iteration - 1];
         } else {
-            // TODO rearchitect this
-            let n = match self.last_spoken.get(&self.prev.unwrap()) {
+            match self.last_spoken.insert(self.prev.unwrap(), self.iteration) {
                 None => {
                     self.prev = Some(0);
-                    0
+                    self.iteration += 1;
+                    return 0;
                 },
-                Some(&i) => {
-                    self.prev = Some(i);
-                    self.iteration - i
+                Some(i) => {
+                    self.prev = Some(self.iteration - i);
+                    self.iteration += 1;
+                    return self.prev.unwrap();
                 }
-            };
-
-            self.iteration += 1;
-            self.last_spoken.insert(n, self.iteration);
-            n
+            }
         }
     }
 }
 
 fn main() -> Result<(), io::Error> {
-    // let file = File::open("input.txt")?;
-    // let reader = BufReader::new(file);
+    // Honestly, why bother reading the file, the input is really small
+    let mut state = State::new(vec![15, 5, 1, 4, 7, 0]);
 
-    // let mut nums: Vec<i32> = vec![];
-
-    // for line in reader.lines() {
-    //     let line = line?;
-    //     let val = line.parse().unwrap();
-    //     nums.push(val);
-    // }
-
-    let mut state = State::new(vec![0, 3, 6]);
-
-    for _ in 0..10 {
-        dbg!(state.step());
+    for i in 0..30000000 {
+        let n = state.step();
+        if i == 2019 {
+            println!("(1) the 2020th number is {}", n);
+        } else if i == 30000000 - 1 {
+            println!("(2) the 30000000th number is {}", n);
+        }
     }
-
-    println!("(1) The 2020th number is {}", state.step());
 
     Ok(())
 }
